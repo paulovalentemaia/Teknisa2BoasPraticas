330a14fbddf302816edc9236230110c1f49cc707
Paulo Valente Maia <paulovalentemaia>
2022-03-30 21:29:13 -0300

    Try Catch

--- exemplo.js
+++ exemplo.js
@@ -1,28 +1,34 @@
-const playerAttack = async(character, enemy) => {
-    if (!getMouseButtonDown()) {
-        //If mouse is not button is not pressed.
-        return;
+const playerAttack = async (character, enemy) => {
+    try {
+        if (!getMouseButtonDown()) {
+            //If mouse is not button is not pressed.
+            return;
+        }
+
+        const movement = getMovement(character.movement);
+
+        if (isSprinting(movement)) {
+            console.log('Cannot attack while sprinting!');
+            return;
+        }
+
+        const action = getAction(character.action);
+
+        if (isBlocking(action)) {
+            console.log('Cannot attack while blocking!');
+            return;
+        }
+
+        character.status = statusCalculation(character);
+        enemy.status = statusCalculation(enemy);
+
+        const damage = await attack(character, enemy);
+        console.log('Character ' + character.name + ' caused ' + damage + ' damage on the enemy ' + enemy.name);
+    } catch (error) {
+        rollbackGame();
+        saveGame();
+        throw error;
     }
-
-    const movement = getMovement(character.movement);
-
-    if (isSprinting(movement)) {
-        console.log('Cannot attack while sprinting!');
-        return;
-    }
-
-    const action = getAction(character.action);
-
-    if (isBlocking(action)) {
-        console.log('Cannot attack while blocking!');
-        return;
-    }
-
-    character.status = statusCalculation(character);
-    enemy.status = statusCalculation(enemy);
-
-    const damage = await attack(character, enemy);
-    console.log('Character ' + character.name + ' caused ' + damage + ' damage on the enemy ' + enemy.name);
 }
 
 //Status Calculation Character and Enemy
@@ -31,7 +37,7 @@ const statusCalculation = (entity) => {
     const defense = entity.defense * entity.level;
     const crit = entity.crit;
 
-    return{
+    return {
         offense,
         defense,
         crit
