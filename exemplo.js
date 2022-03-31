b32f62864294de291e835e2b7dfee9a6a1b96362
Paulo Valente Maia <paulovalentemaia>
2022-03-30 21:23:25 -0300

    Reusabilidade

--- exemplo.js
+++ exemplo.js
@@ -1,8 +1,9 @@
 const playerAttack = async(character, enemy) => {
     if (!getMouseButtonDown()) {
-        //Atack can't occur if mouse isn't pressed.
+        //If mouse is not button is not pressed.
         return;
     }
+
     const movement = getMovement(character.movement);
 
     if (isSprinting(movement)) {
@@ -17,16 +18,22 @@ const playerAttack = async(character, enemy) => {
         return;
     }
 
-    //Character Status Calculation
-    character.status.offense = character.offense * character.level;
-    character.status.defense = character.defense * character.level;
-    character.status.crit = character.critChance;
-
-    //Enemy Status Calculation
-    enemy.status.offense = enemy.offense * enemy.level;
-    enemy.status.defense = enemy.defense * enemy.level;
-    enemy.status.crit = enemy.critChance;
+    character.status = statusCalculation(character);
+    enemy.status = statusCalculation(enemy);
 
     const damage = await attack(character, enemy);
     console.log('Character ' + character.name + ' caused ' + damage + ' damage on the enemy ' + enemy.name);
+}
+
+//Status Calculation Character and Enemy
+const statusCalculation = (entity) => {
+    const offense = entity.offense * entity.level;
+    const defense = entity.defense * entity.level;
+    const crit = entity.crit;
+
+    return{
+        offense,
+        defense,
+        crit
+    }
 }
